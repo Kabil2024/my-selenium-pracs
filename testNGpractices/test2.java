1b9dc29f8323b8f3e7ae8275c10bea9b54974f54
Kabil2024 <kabilarasan44@gmail.com>
2025-02-28 15:27:20 +0530

    new

--- testNGpractices/test2.java
+++ testNGpractices/test2.java
@@ -1,14 +1,49 @@
 package testNGpractices;
 
-import org.testng.annotations.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.*;
 
 public class test2 {
+WebDriver driver;
+//    @AfterMethod
+//    void logout(){
+//
+//        System.out.println("logout");
+//    }
+
+//    @AfterTest
+//    void test(){
+//        System.out.println("test");
+//    }
+//
+//    @AfterClass
+//    void setupClosure(){
+//        driver.quit();
+//        System.out.println("after class");
+//    }
+
+//    @BeforeClass
+//    void beforeSetup(){
+//        driver = new ChromeDriver();
+//        System.out.println("before class");
+//    }
+
+
+
+
     @Test(priority = 1)
-    void tc3(){
+    void homeURLVerification(){
         System.out.println("print tc3");
     }
+
     @Test(priority = 2)
-    void tc4(){
+    void hometitleverification(){
         System.out.println("print tc4");
     }
+//
+//    @BeforeMethod
+//    void login(){
+//        System.out.println("login");
+//    }
 }
